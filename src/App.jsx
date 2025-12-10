c2bb9849b9decf84f3dceb8420ce887ceaced550
josueamador19 <amadoroscar789@gmail.com>
2025-12-09 21:20:06 -0600

    Inicializacion de CronoPlan

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CronoPlan</h1>
+      <p>Frontend en construcción...</p>
+    </div>
+  )
+}
+
+export default App
